400d2dfdd17a2df7dee3d6b08f2195205e835c23
Icey-Glitch <icey-glitch@riseup.net>
2023-09-21 15:39:17 -0700

    comma

--- default.nix
+++ default.nix
@@ -1,11 +1,9 @@
-{config, lib, pkg-config, ...}:
-{
-    pkgs.mpv.overrideAttrs (old: {
-        version = old.version + "-nightly";
 
-        src = pkgs.fetchFromGitHub {
-            owner = "mpv-player";
-            repo = "mpv";
-        };
-    })    
-}
+pkgs.mpv.overrideAttrs (old: {
+    version = old.version + "-nightly";
+
+    src = pkgs.fetchFromGitHub {
+        owner = "mpv-player";
+        repo = "mpv";
+    };
+})    
